15e21f18c94a2ca1339743451ec27255d8cd35fa
Gerard Paligot <gerard.paligot@gmail.com>
2021-03-24 20:38:04 +0100

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by GERARD on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
